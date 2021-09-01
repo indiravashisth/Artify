d4746ed2ce0348db2338447bd66c748919e3097f
Indira Vashisth Medepalli <80642850+indiravashisth@users.noreply.github.com>
2021-09-01 16:48:55 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import { getPosts } from './actions/posts';
 import useStyles from './styles';
-import memories from './images/memories.png';
+import artify from './images/artify.png';
 
 const App = () => {
   const [currentId, setCurrentId] = useState(0);
@@ -21,7 +21,7 @@ const App = () => {
     <Container maxWidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">Artify</Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
+        <img className={classes.image} src={artify} alt="icon" height="60" />
       </AppBar>
       <Grow in>
         <Container>
