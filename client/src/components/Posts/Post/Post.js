7649d79ad3587ca7354d84b28b01f361dada1139
Indira Vashisth Medepalli <80642850+indiravashisth@users.noreply.github.com>
2021-09-13 14:25:28 +0530

    Update Post.js

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -32,7 +32,6 @@ const Post = ({ post, setCurrentId }) => {
     <Card className={classes.card}>
       <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
       <div className={classes.overlay}>
-        <Typography variant="h6">{post.name}</Typography>
         <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
       </div>
       {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
@@ -42,12 +41,10 @@ const Post = ({ post, setCurrentId }) => {
         </Button>
       </div>
       )}
-      <div className={classes.details}>
-        <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
-      </div>
-      <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
+      <CardContent className={classes.details} >
+         <Typography className={classes.title} gutterBottom variant="h6" component="h2">{post.name}</Typography>
+         <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
+         <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
         <Button size="small" color="primary" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}>
