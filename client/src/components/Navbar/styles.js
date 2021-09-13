8d313cdbd492fca1485ba102b29010e2b7c84029
Indira Vashisth Medepalli <80642850+indiravashisth@users.noreply.github.com>
2021-09-13 14:17:34 +0530

    Update styles.js

--- client/src/components/Navbar/styles.js
+++ client/src/components/Navbar/styles.js
@@ -12,7 +12,7 @@ export default makeStyles((theme) => ({
     padding: '10px 50px',
   },
   heading: {
-    color: 'rgba(0,183,255, 1)',
+    color: 'rgba(50,50,50, 1)',
     textDecoration: 'none',
   },
   image: {
